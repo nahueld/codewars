6e4d6de12fb394852b90dec371d7c9ae27f7af90
nahueld <nahuel.dealbera@myob.com>
2017-07-14 16:11:27 +1000

    Adding solution for giveMeChange

--- giveMeChange.js
+++ giveMeChange.js
@@ -1,110 +1,54 @@
+//given a money int and an array of coins calculate how many combinations can
+//you use of coins to return the money
+//example : money 5
+//1+1+1+1+1
+//1+2+2
+//2+1+1+1
+//2+3
+//3+1+1
+//Result then is 5
+
 function countChange(money, coins) {
-  coins = sortCoins(coins);
-  coins = cleanGreater(money, coins);
-  var alternatives = 0;
-  alternatives = recursiveChecker(alternatives,money,coins,coins.slice());
-  return alternatives;
+  var counter = 0;
+  return checkCombinationsWithFactor(0,money,0,coins);
 }
 
-function recursiveChecker(alternatives, money, coins, pristine) {
-  if(coins.length == 0) {
-    return alternatives;
-  } else {
-    var currentCoin = coins.shift();
-    console.log('CURRENT COIN : ' + currentCoin);
-    var pristineWithoutCC = definePristineWithoutCC(currentCoin, pristine);
-    console.log('PRISTINE WITHOUT CURRENT: ', pristineWithoutCC);
-    var arrayOfRepeatedCoins = generateArrayOfCoin(currentCoin, money);
-    console.log('REPEATED COINS: ', arrayOfRepeatedCoins);
-    var currentSum = sumOfThisArr(arrayOfRepeatedCoins);
-    if(currentSum == money) {
-      alternatives++;
-      console.log('Adding self-sufficient array',alternatives);
-    } else if(currentSum < money && coins.length > 0  && pristineWithoutCC.length > 1) {
-      for(var i = 0; i < pristineWithoutCC.length; i++) {
-        var thisSum = currentSum + pristineWithoutCC[i];
-        if(thisSum == money) {
-          alternatives++;
-          console.log('Adding when lower',alternatives);
-        }
+function checkCombinationsWithFactor(currentPosition, target, counter, array) {
+  if(currentPosition >= array.length) {
+    return counter;
+  }else{
+    var curr = array[currentPosition];
+    counter += onItself(curr,target);
+    for(var i = 0; i < array.length; i++) {
+      if(i != currentPosition) {
+        counter += withThisNumber(curr, array[i], target);
       }
     }
-    alternatives = backwardsRecursiveChecker(alternatives, money, arrayOfRepeatedCoins, coins);
-    return recursiveChecker(alternatives, money, coins, pristine);
+    currentPosition++;
+    return checkCombinationsWithFactor(currentPosition,target,counter,array);
   }
 }
 
-function definePristineWithoutCC(cc, pristine) {
-  var res = [];
-  pristine.forEach(function(c) {
-    if(cc != c) res.push(c);
-  });
-  return res;
-}
-
-function backwardsRecursiveChecker(alternatives, money, repeatedCoins, coins) {
-  if(repeatedCoins.length == 0 || coins.length == 0) {
-    return alternatives;
-  } else {
-    repeatedCoins.pop(); //[1,1,1,1]
-    //console.log('Backwards arr: ', repeatedCoins);
-    var summatory = sumOfThisArr(repeatedCoins);
-    for(var i = 0; i < coins.length; i++) {
-      var currentCoin = coins[i];
-      //console.log('CC: ' + currentCoin);
-      var currentSum = summatory + currentCoin;
-      //console.log('Sum: ' + currentSum + ' and trying to get to: ' + money);
-      if(currentSum == money) {
-        alternatives++;
-        console.log('Adding inside backwardsRecursiveChecker', alternatives);
-      }
-    }
-    return backwardsRecursiveChecker(alternatives, money, repeatedCoins, coins);
+function onItself(number, target) {
+  var sum = number;
+  while(sum < target) {
+    sum += number;
   }
+  return sum == target ? 1 : 0;
 }
 
-function sumOfThisArr(arr) {
-  var sum = 0;
-  arr.forEach(function(i) {
-    sum+=i;
-  });
-  return sum;
-}
-
-function generateArrayOfCoin(coin, money) {
-  var res = [];
-  var sum = 0;
-  while (sum <= money) {
-    sum += coin;
-    if(sum <= money) res.push(coin);
+function withThisNumber(current, number, target) {
+  var sum = number;
+  var loops = 0;
+  while(sum < target) {
+    sum += current;
+    loops++;
   }
-  return res;
+  if(loops == 1 && current > number) return 0;
+  return sum == target ? 1 : 0;
 }
 
-function canIgiveChangeWithThis(money, coin) {
-  res = false;
-  return coin == money ? true : calculateIfSummatoryWorks(money, coin);
-}
-
-//ignore
-function calculateIfSummatoryWorks(money, coin) {
-  var summatory = 0;
-  while(summatory < money) {
-    summatory += coin;
-  }
-  return summatory == money;
-}
-
-function cleanGreater(money,coins) {
-  var res = [];
-  coins.forEach(function(c) { if(c <= money) res.push(c); });
-  return res;
-}
-
-function sortCoins(coins) {
-  return coins.sort();
-}
 
 //alternatives, money, repeatedCoins, coins
-console.log(countChange(5,[1,2,3,5]));
+console.log(countChange(5,[1,2,3]));
 //console.log(generateArrayOfCoin(2,5));
